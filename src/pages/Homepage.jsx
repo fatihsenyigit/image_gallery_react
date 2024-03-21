09be189ba6e46da2547b5cf2fa9605ec1c499299
fatih <fsenyigit8@gmail.com>
2024-03-21 22:15:48 +0300

    key warning handled

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,19 +3,19 @@ import data from '../helper/data'
 
 function Homepage() {
   return (
-    <div className='homePage'>
-        {data.map((e)=>{
-            return (
-              <div>
-                <div className="img-container">
-                  <img src={e.src.large} alt="e.alt" />
-                </div>
-                <p>{e.photographer}</p>
-              </div>
-            );
-        })}
+    <div className="homePage">
+      {data.map((e, index) => {
+        return (
+          <div key={index}>
+            <div className="img-container">
+              <img src={e.src.large} alt="e.alt" />
+            </div>
+            <p>{e.photographer}</p>
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
 
 export default Homepage
